0e1525e147ea47ff558cb9a44c80643ada428c5c
Herbert Yeung <debouncing@gmail.com>
2017-09-12 23:04:58 +1000

    Support for javascript StaticResource for Non-Linear Overlay. Support for IFrameResource & HTMLResource for Non-Linear Overlays. Support for TrackingEvents (like Companion Ads).

--- src/scripts/ads/vast/NonLinear.js
+++ src/scripts/ads/vast/NonLinear.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var TrackingEvent = require('./TrackingEvent');
+
+var utilities = require('../../utils/utilityFunctions');
+
 var xml = require('../../utils/xml');
 
 function NonLinear(nonLinearJTree) {
@@ -12,6 +16,26 @@ function NonLinear(nonLinearJTree) {
   this.staticResource = xml.keyValue(nonLinearJTree.staticResource);
   this.nonLinearClickThrough = xml.keyValue(nonLinearJTree.nonLinearClickThrough);
 
+  // Dynamic resource(s) iFrame and html
+  // Derived from Companion.js
+  var htmlResource = null;
+  if (xml.keyValue(nonLinearJTree.HTMLResource)) {
+    htmlResource = xml.keyValue(nonLinearJTree.HTMLResource);
+  } else if (xml.keyValue(nonLinearJTree.hTMLResource)) {
+    htmlResource = xml.keyValue(nonLinearJTree.hTMLResource);
+  }
+  this.htmlResource = htmlResource;
+
+  var iframeResource = null;
+  if (xml.keyValue(nonLinearJTree.IFrameResource)) {
+    iframeResource = xml.keyValue(nonLinearJTree.IFrameResource);
+  } else if (xml.keyValue(nonLinearJTree.iFrameresource)) {
+    iframeResource = xml.keyValue(nonLinearJTree.iFrameresource);
+  } else if (xml.keyValue(nonLinearJTree.iFrameResource)) {
+    iframeResource = xml.keyValue(nonLinearJTree.iFrameResource);
+  }
+  this.iframeResource = iframeResource;
+
   // optional
   this.id = xml.attr(nonLinearJTree, 'id');
   this.width = xml.attr(nonLinearJTree, 'width');
@@ -20,6 +44,23 @@ function NonLinear(nonLinearJTree) {
   this.scalable = xml.attr(nonLinearJTree, 'scalable');
   this.minSuggestedDuration = xml.attr(nonLinearJTree, 'minSuggestedDuration');
   this.nonLinearClickTracking = xml.keyValue(nonLinearJTree.nonLinearClickTracking);
+  this.expandedWidth = xml.attr(nonLinearJTree, 'expandedWidth');
+  this.expandedHeight = xml.attr(nonLinearJTree, 'expandedHeight');
+  this.apiFramework = xml.attr(nonLinearJTree, 'apiFramework');
+  this.adParameters = xml.attr(nonLinearJTree, 'adParameters');
+  this.trackingEvents = parseTrackingEvents(nonLinearJTree.trackingEvents && nonLinearJTree.trackingEvents.tracking);
+
+  /*** Local functions ***/
+  function parseTrackingEvents(trackingEvents) {
+    var trackings = [];
+    if (utilities.isDefined(trackingEvents)) {
+      trackingEvents = utilities.isArray(trackingEvents) ? trackingEvents : [trackingEvents];
+      trackingEvents.forEach(function (trackingData) {
+        trackings.push(new TrackingEvent(trackingData));
+      });
+    }
+    return trackings;
+  }
 }
 
 NonLinear.prototype.isSupported = function () {
